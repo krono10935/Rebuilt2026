43d939ac8857464a7e33e5c26cdd5ab302bcbffc
Lior Margalit <lior.2010.margalit@gmail.com>
2026-01-29 17:04:10 +0200

    Finished Sequences need to add isFinished Command to intake

--- src/main/java/frc/robot/Sequences.java
+++ src/main/java/frc/robot/Sequences.java
@@ -13,33 +13,32 @@ import frc.robot.subsystems.Indexer.Indexer;
 import frc.robot.subsystems.Shooter.Shooter;
 import frc.robot.subsystems.climb.Climb;
 import frc.robot.subsystems.climb.ClimbConstants;
+import frc.robot.subsystems.drivetrain.Drivetrain;
 import frc.robot.subsystems.intake.Intake;
 import frc.robot.subsystems.intake.IntakeConstants;
 
 public class Sequences {
-    //TODO fix the sequences
-    public static Command shoot(Shooter shooter, Indexer indexer) {
+    public static Command shoot(Shooter shooter, Indexer indexer,Drivetrain drivetrain ) {
         SequentialCommandGroup shooterCommand = new SequentialCommandGroup();
-        //shooterCommand.addCommands(new HomeHoodCommand())
+        shooterCommand.addCommands(new HomeHoodCommand(shooter, drivetrain::getEstimatedPosition,(pose) -> Rotation2d.kZero));
         shooterCommand.addCommands(new SpinUp(shooter).alongWith(indexer.turnOnIndexerCommand()));
-        //shooterCommand.addCommandshootCommand().alongWith);
+        shooterCommand.addCommands(new ShootCommand(shooter,drivetrain::getEstimatedPosition,(pose) -> true));
         return shooterCommand;
     }
 
-    public static Command delivery(Shooter shooter, Indexer indexer, Intake intake) {
+    public static Command delivery(Shooter shooter, Indexer indexer, Intake intake, Drivetrain drivetrain,IntakeConstants intakeConstants ) {
         SequentialCommandGroup deliveryCommand = new SequentialCommandGroup();
-        deliveryCommand.addCommands(new IntakeCommand(intake));
-        //deliveryCommand.addCommands(new HomeHoodCommand()); set hood degree
-        deliveryCommand.addCommands(Sequences.shoot(shooter, indexer));
+        deliveryCommand.addCommands(Sequences.openIntakeStart(intake,intakeConstants));
+        deliveryCommand.addCommands(new HomeHoodCommand(shooter, drivetrain::getEstimatedPosition,(pose) -> Rotation2d.kZero));
+        deliveryCommand.addCommands(Sequences.shoot(shooter,indexer,drivetrain));
         return deliveryCommand;
     }
 
     public static Command Climb(Intake intake, IntakeConstants intakeConstands, Climb climb,Indexer indexer){
         SequentialCommandGroup climbCommand = new SequentialCommandGroup();
-        climbCommand.addCommands(new CloseCommand(intake,intakeConstands));
-        climbCommand.addCommands(indexer.turnOffIndexerCommand());
         if(climb.getClimbState() == ClimbConstants.ClimbState.CLOSED) {
-
+            climbCommand.addCommands(Sequences.closeIntakeStop(intake,intakeConstands));
+            climbCommand.addCommands(indexer.turnOffIndexerCommand());
             climbCommand.addCommands(climb.openCommand());
         }else if(climb.getClimbState() == ClimbConstants.ClimbState.OPEN) {
             climbCommand.addCommands(climb.closeCommand());
@@ -47,9 +46,9 @@ public class Sequences {
         return climbCommand;
     }
 
-    public static Command openIntakeStart(Intake intake, IntakeConstants intakeConstands){
+    public static Command openIntakeStart(Intake intake, IntakeConstants intakeConstants){
         SequentialCommandGroup intakeCommand = new SequentialCommandGroup();
-        intakeCommand.addCommands(new OpenCommand(intake,intakeConstands));
+        intakeCommand.addCommands(new OpenCommand(intake,intakeConstants));
         intakeCommand.addCommands(new IntakeCommand(intake));
         return intakeCommand;
     }
@@ -57,7 +56,6 @@ public class Sequences {
     public static Command closeIntakeStop(Intake intake, IntakeConstants intakeConstands){
         SequentialCommandGroup intakeCommand = new SequentialCommandGroup();
         intakeCommand.addCommands(new CloseCommand(intake,intakeConstands));
-        // TODO change to stop intake
         intakeCommand.addCommands(new IntakeCommand(intake));
         return intakeCommand;
     }
