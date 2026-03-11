da8b0b33d8d00ffbf4f5c815996786e99a395fe1
Roni <roniyaakobi@yahoo.com>
2026-03-11 12:57:20 +0200

    Added smartdashboard widget to indicate how full the logging drive is

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -11,12 +11,14 @@ import com.revrobotics.util.StatusLogger;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.RobotState;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.subsystems.Shooter.ShotCalculator;
+import frc.utils.CheckFreeSpace;
 import frc.utils.Elastic;
 import frc.utils.ModeFileHandling;
 import frc.utils.SwitchedToPitModeException;
@@ -70,6 +72,8 @@ public class Robot extends LoggedRobot
         CommandScheduler.getInstance().schedule(PathfindingCommand.warmupCommand());
 
         new Trigger(()-> DriverStation.isDSAttached()).onTrue(new InstantCommand(() -> Elastic.selectTab("Autonomous")));
+
+        SmartDashboard.putNumber("Robot/Disk Used Space Percent", CheckFreeSpace.checkUsedPercentage());
     }
 
 
