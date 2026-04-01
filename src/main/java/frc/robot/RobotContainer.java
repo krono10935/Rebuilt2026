2b160d1477390a2f3a2e47455ad67b8a8b5f66dd
captainsoccer <justeyallevi@gmail.com>
2026-04-02 00:04:59 +0300

    Revert "Added control scheme changes as requested by drivers"
    This reverts commit 5035a26c3e79b4a0067ef34b1e7885208903ebab.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -70,8 +70,6 @@ public class RobotContainer {
 
     private final CommandXboxController operatorController;
 
-    private final CommandXboxController intakeController;
-
     public final Drivetrain drivetrain;
 
     private final LoggedDashboardChooser<Command> autoChooser;
@@ -106,8 +104,6 @@ public class RobotContainer {
         driverController = new CommandXboxController(0);
 
         operatorController = new CommandXboxController(1);
-
-        intakeController = new CommandXboxController(2);
 
         vision = new Vision(drivetrain::addVisionMeasurement, drivetrain::getEstimatedPosition);
 
@@ -285,6 +281,9 @@ public class RobotContainer {
 
         //operatorController.a().whileTrue(ShootCommand.shootCommandFactory(shooter, drivetrain, driverController, intake, vision, operatorController.y(), () -> currentIntakeMode));
 
+
+        driverController.y().toggleOnTrue(new DriveAndHomeToHubCommand(drivetrain, driverController));
+
         //autoShoot.whileTrue(Commands.print("autoshoot"));
 
 
@@ -335,11 +334,11 @@ public class RobotContainer {
 
         var shootCommand = ShootCommand.operatorShootCommandFactory(
                                 shooter, drivetrain, vision, intake, operatorController.y() ,() -> currentIntakeMode,
-                                () -> overrideShooting, operatorController.b(), driverController);
+                                () -> overrideShooting, operatorController.b());
 
         var immediateShootCommand = ShootCommand.operatorShootCommandFactory(
                                 shooter, drivetrain, vision, intake, operatorController.y() ,() -> currentIntakeMode,
-                                () -> overrideShooting, operatorController.b(), driverController).onlyIf(()-> !shootCommand.isScheduled());
+                                () -> overrideShooting, operatorController.b()).onlyIf(()-> !shootCommand.isScheduled());
         
         operatorController.b().onTrue(new CloseCommand(intake).onlyIf(() -> 
                 !shootCommand.isScheduled() && 
@@ -369,10 +368,6 @@ public class RobotContainer {
          () -> shooter.getIndexer().turnOn(), shooter.getIndexer())
          .onlyIf(() -> shooter.getIndexer().getCurrentCommand() == null));
 
-        intakeController.a().onTrue(new InstantCommand(() -> currentIntakeMode = IntakeMode.TenBalls));
-        intakeController.b().onTrue(new InstantCommand(() -> currentIntakeMode = IntakeMode.TwentyBalls));
-        intakeController.y().onTrue(new InstantCommand(() -> currentIntakeMode = IntakeMode.ThirtyBalls));
-        intakeController.x().onTrue(new InstantCommand(() -> currentIntakeMode = IntakeMode.FourtyBalls));
 
 
     }
