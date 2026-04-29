1bb705276a71d6f7c49ec60bf86194b81cb782a9
captainsoccer <justeyallevi@gmail.com>
2026-04-29 11:52:30 -0500

    operator and fms auto won indicator is now a text of who won, and there is an indicator for whether or not the fms said who won

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -22,13 +22,19 @@ public class Constants {
     public static final Mode currentMode = RobotBase.isReal() ? Mode.REAL : simMode;
 
     public static enum Mode {
-        /** Running on a real robot. */
+        /**
+         * Running on a real robot.
+         */
         REAL,
 
-        /** Running a physics simulator. */
+        /**
+         * Running a physics simulator.
+         */
         SIM,
 
-        /** Replaying from a log file. */
+        /**
+         * Replaying from a log file.
+         */
         REPLAY
     }
 
@@ -40,11 +46,11 @@ public class Constants {
 
     public static final double HUB_ACTIVITY_DEABAND_BEFORE_ACTIVE = 2;
 
-    public static final ProfiledPIDController THETA_CONTROLLER = 
-        new ProfiledPIDController(4,8,0.1,
-        new Constraints(10, 5));
+    public static final ProfiledPIDController THETA_CONTROLLER =
+            new ProfiledPIDController(4, 8, 0.1,
+                    new Constraints(10, 5));
 
-    static{
+    static {
         THETA_CONTROLLER.enableContinuousInput(-Math.PI, Math.PI);
         THETA_CONTROLLER.setIntegratorRange(-10, 10);
         THETA_CONTROLLER.setIZone(Rotation2d.fromDegrees(20).getRadians());
@@ -52,111 +58,112 @@ public class Constants {
         SmartDashboard.putData(THETA_CONTROLLER);
     }
 
-    public enum Phase{
-            AUTO(0 ,0),
-            TranistionShift(140,130.1),
-            FirstShift(130,105.1),
-            SecondShift(105,80.1),
-            ThirdShift(80,55.1),
-            FourthShift(55,30.1),
-            EndGame(30,0.1),
-            Invalid(0,0);
+    public enum Phase {
+        AUTO(0, 0),
+        TranistionShift(140, 130.1),
+        FirstShift(130, 105.1),
+        SecondShift(105, 80.1),
+        ThirdShift(80, 55.1),
+        FourthShift(55, 30.1),
+        EndGame(30, 0.1),
+        Invalid(0, 0);
 
 
-            public final double STARTING_TIME;
-            public final double FINSIHING_TIME;
-            
-            Phase(double StartingTime, double FinishingTime){
-                STARTING_TIME = StartingTime;
-                FINSIHING_TIME = FinishingTime;
-            }
+        public final double STARTING_TIME;
+        public final double FINSIHING_TIME;
 
-            public double timeToShiftEnd(double time){
-                return time - FINSIHING_TIME;
-            }
+        Phase(double StartingTime, double FinishingTime) {
+            STARTING_TIME = StartingTime;
+            FINSIHING_TIME = FinishingTime;
+        }
 
-            public static Phase getActivePhase(double time){
-                if(RobotState.isAutonomous()) return AUTO;
+        public double timeToShiftEnd(double time) {
+            return time - FINSIHING_TIME;
+        }
 
-                for (Phase phase : Phase.values()){
-                    if ((time <= phase.STARTING_TIME) && (time >= phase.FINSIHING_TIME)){
-                        return phase;
-                    }
+        public static Phase getActivePhase(double time) {
+            if (RobotState.isAutonomous()) return AUTO;
+
+            for (Phase phase : Phase.values()) {
+                if ((time <= phase.STARTING_TIME) && (time >= phase.FINSIHING_TIME)) {
+                    return phase;
                 }
-
-                return Invalid;
             }
+
+            return Invalid;
+        }
     }
 
-    public static class HubTiming{
-        
-        private static Boolean isActiveFirst = null;
-        private static Boolean isActiveFirstHuman = null;
+    public static class HubTiming {
 
-        public enum DataSource{
+        private static boolean isActiveFirst = false;
+        private static DataSource dataSource = DataSource.CODE;
+
+        public enum DataSource {
             FMS,
             HUMAN,
             CODE;
+        }
 
-            @AutoLogOutput(key = "Hubtiming/Datasource")
-            private static DataSource current = CODE;
-
-            public static DataSource getDataSource(){
-                return current;
-            }
+        static {
+            SmartDashboard.putString("active first status", "Not decided");
+            SmartDashboard.putBoolean("fms said active first", false);
         }
 
         /**
-         * 
-         * @param team Get from driverstations game specific message the team
+         *
+         * @param team     Get from driverstations game specific message the team
          * @param alliance Get from driverstation
          */
-        public static void setStartingTeam(String team, Alliance alliance){
+        public static void setStartingTeam(String team, Alliance alliance) {
             boolean isRed = alliance == Alliance.Red;
 
-            if ((team.charAt(0) == 'R' && isRed) || (team.charAt(0) == 'B' && !isRed)){
+            if ((team.charAt(0) == 'R' && isRed) || (team.charAt(0) == 'B' && !isRed)) {
                 isActiveFirst = false;
             } else {
                 isActiveFirst = true;
             }
 
-           DataSource.current = DataSource.FMS;
+            dataSource = DataSource.FMS;
+            String text = isActiveFirst ? "We lost" : "We won";
+            SmartDashboard.putBoolean("fms said active first", true);
+            SmartDashboard.putString("active first status", text);
         }
 
-        public static Boolean getAutoIsActiveDetection(){
+        public static Boolean getAutoIsActiveDetection() {
             return isActiveFirst;
         }
 
-        public static void setHumanActiveFirst(boolean isActiveFirstHumanInput){
-            isActiveFirstHuman = isActiveFirstHumanInput;
-            DataSource.current = DataSource.getDataSource() != DataSource.FMS ? DataSource.FMS: DataSource.HUMAN;
+        public static void setHumanActiveFirst(boolean isActiveFirstHumanInput) {
+            if (dataSource == DataSource.FMS)
+                return;
+
+            dataSource = DataSource.HUMAN;
+            isActiveFirst = isActiveFirstHumanInput;
+            String text = isActiveFirst ? "We lost" : "We won";
+            SmartDashboard.putString("active first status", text);
         }
 
-        public static double timeRemainingToShift(double time){
+        public static double timeRemainingToShift(double time) {
             Phase phase = Phase.getActivePhase(time);
             return time - phase.FINSIHING_TIME;
         }
-        public static boolean isActiveFirst(){
-            boolean currentIsActiveFirst = false;
-            if (isActiveFirst != null) currentIsActiveFirst = isActiveFirst;
-            if (isActiveFirstHuman != null) currentIsActiveFirst = isActiveFirstHuman;
 
-            Logger.recordOutput("isActiveFirst", currentIsActiveFirst);
-
-            return currentIsActiveFirst;
+        public static boolean isActiveFirst() {
+            return isActiveFirst;
         }
 
-        public static double timeToNextShift(double time){
+        public static double timeToNextShift(double time) {
             return Phase.getActivePhase(time).timeToShiftEnd(time);
         }
 
         /**
-         * 
+         *
          * @param time time to check if the hub would be activated during
          * @return whether or not at a given timestamp the hub would be active for your team
          */
-        public static boolean isActive(double time){
-             Phase phase = Phase.getActivePhase(time);
+        public static boolean isActive(double time) {
+            Phase phase = Phase.getActivePhase(time);
             return switch (phase) {
                 case AUTO, TranistionShift, EndGame -> true;
                 case SecondShift, FourthShift ->
@@ -166,17 +173,17 @@ public class Constants {
                 case FirstShift, ThirdShift -> !isActiveFirst();
                 default -> false;
             };
-            
+
         }
 
         /**
-         * 
-         * @param time time to check if the hub would be activated during
+         *
+         * @param time       time to check if the hub would be activated during
          * @param timeBefore time before the timestamp to check activity
-         * @param timeAfter time after the timestamp to check activity
+         * @param timeAfter  time after the timestamp to check activity
          * @return whether or not at a given timestamp the hub would be active for your team
          */
-        public static boolean isActiveWithMargin(double time, double timeBefore, double timeAfter){
+        public static boolean isActiveWithMargin(double time, double timeBefore, double timeAfter) {
             return isActive(time + timeBefore) || isActive(time) || isActive(time - timeAfter);
         }
     }
